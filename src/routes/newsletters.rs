61ae5679871db3914ac7b223fe3b358af1966a1a
Brayan <Brayanjaviergomez.g@gmail.com>
2025-03-19 21:17:32 -0500

    feat add newsletter implementation

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,8 +1,11 @@
 //! src/routes/newsletters.rs
+use actix_web::http::header::{self, HeaderMap, HeaderValue};
 use actix_web::web;
+use actix_web::HttpRequest;
 use actix_web::HttpResponse;
 use actix_web::{http::StatusCode, ResponseError};
 use anyhow::Context;
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::domain::subscriber_email::SubscriberEmail;
@@ -28,6 +31,10 @@ struct ConfirmedSubscriber {
 //Manejadores de error:
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    // Nueva variante de error
+    #[error("La autenticación ha fallado.")]
+    AuthError(#[source] anyhow::Error),
+
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -41,9 +48,23 @@ impl std::fmt::Debug for PublishError {
 
 // Definimos cómo `PublishError` se traduce en un código de estado HTTP
 impl ResponseError for PublishError {
-    fn status_code(&self) -> StatusCode {
+    fn error_response(&self) -> HttpResponse {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
+                
+                response
+                    .headers_mut()
+                    // `actix_web::http::header` proporciona una colección de constantes
+                    // para los nombres de varios encabezados HTTP estándar
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+                
+                response
+            }
         }
     }
 }
@@ -55,12 +76,22 @@ impl std::fmt::Display for SubscriberEmail {
     }
 }
 
-//Implementacion de envio de correos
+
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+
+//Implementacion principal de envio de correos
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest
 ) -> Result<HttpResponse, PublishError> {
+    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
     let subscribers = get_confirmed_subscribers(&pool).await?;
 
     for subscriber in subscribers {
@@ -112,4 +143,42 @@ async fn get_confirmed_subscribers(
     })
     .collect();
     Ok(confirmed_subscribers)
+}
+
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // El valor del encabezado, si está presente, debe ser una cadena UTF8 válida
+    let header_value = headers
+        .get("Authorization")
+        .context("El encabezado 'Authorization' no estaba presente")?
+        .to_str()
+        .context("El encabezado 'Authorization' no era una cadena UTF8 válida.")?;
+    
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("El esquema de autorización no era 'Basic'.")?;
+    
+    let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
+        .context("Error al decodificar en base64 las credenciales 'Basic'.")?;
+    
+    let decoded_credentials = String::from_utf8(decoded_bytes)
+        .context("La cadena de credenciales decodificada no es UTF8 válida.")?;
+    
+    // Dividir en dos segmentos, usando ':' como delimitador
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("Se debe proporcionar un nombre de usuario en la autenticación 'Basic'."))?
+        .to_string();
+    
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("Se debe proporcionar una contraseña en la autenticación 'Basic'."))?
+        .to_string();
+    
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
